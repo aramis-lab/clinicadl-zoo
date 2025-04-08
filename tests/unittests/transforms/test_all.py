3e85ea583536e13097165558a6f196a52fab935f
thibaultdvx <thibault.devarax@icm-institute.org>
2025-04-08 18:07:35 +0200

    put test_get_transform_config in test_all.py

--- tests/unittests/transforms/test_all.py
+++ tests/unittests/transforms/test_all.py
@@ -1,15 +1,25 @@
-import pytest
 import torch
 import torchio as tio
+from clinicadl.data.structures import DataPoint
 from clinicadl.transforms.config import TransformConfig
 
 from clinicadl_zoo.transforms import ZooTransform
 from clinicadl_zoo.transforms.factory import get_transform_config
 
-X = tio.Subject(
+X = DataPoint(
     image=tio.ScalarImage(tensor=torch.randn(1, 16, 17, 18)),
     label=tio.LabelMap(tensor=torch.ones(1, 16, 17, 18)),
+    participant="",
+    session="",
 )
+
+
+def test_get_transform_config():
+    config = get_transform_config("NanRemoval", nan=1)
+    assert config.name == "NanRemoval"
+    assert config.nan == 1
+    assert config.posinf is None
+    assert config.neginf is None
 
 
 def test_all_config():
@@ -36,4 +46,4 @@ def test_all_transforms():
 
         transform_obj = config.get_object()
         assert isinstance(transform_obj, tio.Transform)
-        assert isinstance(transform_obj(X), tio.Subject)
+        assert isinstance(transform_obj(X), DataPoint)
